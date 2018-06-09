2b4eff420958f8ed20db7e129eaca076064aed50
Emil Gonzalez <emilrichardo@gmail.com>
2018-06-09 13:21:08 -0300

    Add filter field and search

--- employee_listing.php
+++ employee_listing.php
@@ -27,6 +27,31 @@
         <div class="card">
           <div class="card-body">
             <a href="#" class="btn btn-lg btn-light"> <i class="fa fa-plus"></i> Add employed</a>
+            <div class="filters ml-5 d-inline-block">
+              <div class="row">
+                <div class="col-md-4">
+                  <input type="text" class="form-control" placeholder="Buscar">
+                </div>
+                <div class="col-md-4">
+                  <select class="form-control" id="exampleFormControlSelect1">
+                    <option>Filter</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </select>
+                </div>
+                <div class="col-md-4">
+                  <select class="form-control" id="exampleFormControlSelect1">
+                    <option>Filter</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </select>
+                </div>
+              </div>
+            </div><!-- filter -->
           </div>
         </div>
 
